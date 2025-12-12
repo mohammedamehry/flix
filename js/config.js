869eb5779b5b9e499735b4549b40392fbcffd769
AMEHRY <74459567+mohammedamehry@users.noreply.github.com>
2025-12-12 16:11:46 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,6 +3,6 @@ const CONFIG = {
     // 2. FOR PRODUCTION (GitHub Pages): Replace 'https://api.yourdomain.com' with your EC2 Public IP or Domain
     //    Example: 'http://54.123.45.67:3000' (if using HTTP) or 'https://api.myapp.com' (if using HTTPS)
     BACKEND_URL: (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1')
-        ? 'http://localhost:3000'
-        : 'https://api.yourdomain.com'
+        ? 'http://44.220.144.161/:3000'
+        : 'https://proxy.flixmax.to'
 };
